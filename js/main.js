ed0582f1a5aa75a3216e5db2d2aff5ba1c5ff28f
Alexey <alexey-lopatenko@yandex.ru>
2022-05-22 21:26:08 +0300

    Finished the desktop part

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 $(function() {
-    $('.bike__slider').slick({
+    $('.bike__slider, .slider__item').slick({
         arrows: false,
         dots: true,
         fade: true,
         autoplay: true,
         autoplaySpeed: 2000
     });
+
+    $(".menu, .bike__column").on("click","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
 });
